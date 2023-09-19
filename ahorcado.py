e76dd4fcbf9c05213ce288671dc6d43405cdde8d
Alexis Jeriha <alexis.jeriha@gmail.com>
2023-09-18 22:12:02 -0300

    arriesgar letra mejorado

--- ahorcado.py
+++ ahorcado.py
@@ -1,15 +1,34 @@
 class Ahorcado:
-    def __init__ (self):
-        self.palabra = self.generar_palabra()
+    def __init__ (self, palabra):
+        self.palabra = palabra
+        self.estado = self.inicializar_espacios()
+        self.vidas = 6
 
-    def generar_palabra(self):
-        return "palabra"
-
-    def arriesgar_letra(self, letra):
+    def letra_pertenece(self,letra):
         return letra in self.palabra
+    
+    def arriesgar_letra(self, letra):
+        if self.letra_pertenece(letra):
+            nuevo_estado = self.estado.replace(" ", "")
+            for i,char in enumerate(self.palabra):
+                if(char == letra):
+                    nuevo_estado = list(nuevo_estado)
+                    nuevo_estado[i] = letra
+            nuevo_estado = " ".join(nuevo_estado)
+            
+            self.estado = nuevo_estado
+        else:
+            self.restar_vidas()
 
     def arriesgar_palabra(self, palabra):
         return palabra == self.palabra
 
-    def mostrar_espacios(self):
+    def inicializar_espacios(self):
         return ("_ " * len(self.palabra)).rstrip()
+    
+    # def actualizar_estado(self, letra):
+    
+    # def juego_perdido(self):
+    
+    def restar_vidas(self):
+        self.vidas -= 1
--- tests.py
+++ tests.py
@@ -2,25 +2,46 @@ import unittest
 from ahorcado import Ahorcado
 class Test(unittest.TestCase):
 
-    def test_arriesgar_letra_y_adivinar(self):
-       ahorcado = Ahorcado()
-       self.assertTrue(ahorcado.arriesgar_letra("b"))
+   def test_letra_pertenece_y_adivinar(self):
+      ahorcado = Ahorcado("palabra")
+      self.assertTrue(ahorcado.letra_pertenece("b"))
 
-    def test_arriesgar_letra_y_errar(self):
-       ahorcado = Ahorcado()
-       self.assertFalse(ahorcado.arriesgar_letra("e"))
+   def test_letra_pertenece_y_errar(self):
+      ahorcado = Ahorcado("palabra")
+      self.assertFalse(ahorcado.letra_pertenece("e"))
 
-    def test_arriesgar_palabra_y_adivinar(self):
-       ahorcado = Ahorcado()
-       self.assertTrue(ahorcado.arriesgar_palabra("palabra"))
+   def test_arriesgar_letra_y_errar(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.arriesgar_letra("e")
+      self.assertEqual(ahorcado.estado, "_ _ _ _ _ _ _")
+      
+   def test_arriesgar_letra_unica_y_acertar(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.arriesgar_letra("p")
+      self.assertEqual(ahorcado.estado, "p _ _ _ _ _ _")
+   
+   def test_arriesgar_letra_multiple_y_acertar(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.arriesgar_letra("a")
+      self.assertEqual(ahorcado.estado, "_ a _ a _ _ a")
 
-    def test_arriesgar_palabra_y_errar(self):
-       ahorcado = Ahorcado()
-       self.assertFalse(ahorcado.arriesgar_palabra("otrapalabra"))
+   def test_arriesgar_palabra_y_adivinar(self):
+      ahorcado = Ahorcado("palabra")
+      self.assertTrue(ahorcado.arriesgar_palabra("palabra"))
 
-    def test_mostrar_espacios(self):
-       ahorcado = Ahorcado()
-       self.assertEqual(ahorcado.mostrar_espacios(), "_ _ _ _ _ _ _")
+   def test_arriesgar_palabra_y_errar(self):
+      ahorcado = Ahorcado("palabra")
+      self.assertFalse(ahorcado.arriesgar_palabra("otrapalabra"))
 
+   def test_inicializar_espacios(self):
+      ahorcado = Ahorcado("palabra")
+      self.assertEqual(ahorcado.inicializar_espacios(), "_ _ _ _ _ _ _")
+
+   def test_actualizar_vidas(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.restar_vidas()
+      self.assertEqual(ahorcado.vidas, 5)
+   
+   
 if __name__ == '__main__':
     unittest.main()
