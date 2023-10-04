acf6d22b1ce2f90667701703426747a9fdb64261
Alexis Jeriha <alexis.jeriha@gmail.com>
2023-10-03 21:01:13 -0300

    arreglar letra refactor y actualizar estado

--- ahorcado.py
+++ ahorcado.py
@@ -1,24 +1,51 @@
 class Ahorcado:
+
+    ESTADOS = {
+        "DERROTA": -1,
+        "EN_JUEGO": 0,
+        "VICTORIA": 1,
+    }
+
     def __init__ (self, palabra):
         self.palabra = palabra
-        self.estado = self.inicializar_espacios()
+        self.slots = self.inicializar_espacios()
+        self.estado = self.ESTADOS["EN_JUEGO"]
         self.vidas = 6
+
+    def loop_principal(self):
+            while(self.estado == self.ESTADOS["EN_JUEGO"]):
+                letra = input("Ingrese letra: ")
+                self.arriesgar_letra(letra)
+                self.actualizar_estado_juego()
+                print(self.slots)
+        # self.finaliza_juego()
 
     def letra_pertenece(self,letra):
         return letra in self.palabra
     
     def arriesgar_letra(self, letra):
         if self.letra_pertenece(letra):
-            nuevo_estado = self.estado.replace(" ", "")
-            for i,char in enumerate(self.palabra):
-                if(char == letra):
-                    nuevo_estado = list(nuevo_estado)
-                    nuevo_estado[i] = letra
-            nuevo_estado = " ".join(nuevo_estado)
-            
-            self.estado = nuevo_estado
+            self.reemplazar_letra(letra)
         else:
             self.restar_vidas()
+
+    def reemplazar_letra(self, letra):
+        slots_sin_espacios = self.slots.replace(" ", "")
+        for i,char in enumerate(self.palabra):
+            if(char == letra):
+                slots_sin_espacios = list(slots_sin_espacios)
+                slots_sin_espacios[i] = letra
+        slots_sin_espacios = " ".join(slots_sin_espacios)
+        self.slots = slots_sin_espacios
+    
+    def partida_en_juego(self):
+        return self.vidas > 0 and self.estado == self.ESTADOS["EN_JUEGO"] 
+    
+    def actualizar_estado_juego(self):
+        if self.vidas == 0:
+            self.estado = self.ESTADOS["DERROTA"]
+        elif not "_" in self.slots: 
+            self.estado = self.ESTADOS["VICTORIA"]
 
     def arriesgar_palabra(self, palabra):
         return palabra == self.palabra
--- tests.py
+++ tests.py
@@ -13,17 +13,17 @@ class Test(unittest.TestCase):
    def test_arriesgar_letra_y_errar(self):
       ahorcado = Ahorcado("palabra")
       ahorcado.arriesgar_letra("e")
-      self.assertEqual(ahorcado.estado, "_ _ _ _ _ _ _")
+      self.assertEqual(ahorcado.slots, "_ _ _ _ _ _ _")
       
    def test_arriesgar_letra_unica_y_acertar(self):
       ahorcado = Ahorcado("palabra")
       ahorcado.arriesgar_letra("p")
-      self.assertEqual(ahorcado.estado, "p _ _ _ _ _ _")
+      self.assertEqual(ahorcado.slots, "p _ _ _ _ _ _")
    
    def test_arriesgar_letra_multiple_y_acertar(self):
       ahorcado = Ahorcado("palabra")
       ahorcado.arriesgar_letra("a")
-      self.assertEqual(ahorcado.estado, "_ a _ a _ _ a")
+      self.assertEqual(ahorcado.slots, "_ a _ a _ _ a")
 
    def test_arriesgar_palabra_y_adivinar(self):
       ahorcado = Ahorcado("palabra")
@@ -39,9 +39,32 @@ class Test(unittest.TestCase):
 
    def test_actualizar_vidas(self):
       ahorcado = Ahorcado("palabra")
+      vidas_actual = ahorcado.vidas
       ahorcado.restar_vidas()
-      self.assertEqual(ahorcado.vidas, 5)
+      self.assertEqual(ahorcado.vidas, vidas_actual - 1)
+      
+   def test_actualizar_estado_juego_y_ganar(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.vidas = 6
+      ahorcado.arriesgar_letra("p")
+      ahorcado.arriesgar_letra("a")
+      ahorcado.arriesgar_letra("l")
+      ahorcado.arriesgar_letra("r")
+      ahorcado.arriesgar_letra("b")
+      ahorcado.actualizar_estado_juego()
+      self.assertEqual(ahorcado.estado, ahorcado.ESTADOS["VICTORIA"])
    
+   def test_actualizar_estado_juego_y_perder(self):
+      ahorcado = Ahorcado("palabra")
+      ahorcado.vidas = 6
+      ahorcado.arriesgar_letra("z")
+      ahorcado.arriesgar_letra("x")
+      ahorcado.arriesgar_letra("o")
+      ahorcado.arriesgar_letra("ñ")
+      ahorcado.arriesgar_letra("w")
+      ahorcado.arriesgar_letra("m")
+      ahorcado.actualizar_estado_juego()
+      self.assertEqual(ahorcado.estado, ahorcado.ESTADOS["DERROTA"])
    
 if __name__ == '__main__':
     unittest.main()
