6692332493582efef6d96d7853b426b54e035aef
Alexis Jeriha <alexis.jeriha@gmail.com>
2023-11-07 01:35:54 -0300

    update tests and results

--- automation/features/environment.py
+++ automation/features/environment.py
@@ -7,18 +7,9 @@ ahorcado_link = 'https://www.youtube.com/'
 
 def before_all(context):
    print('Iniciando ATDD')
-   context.allure_report_dir = 'allure-reports'
    context.driver = webdriver.Chrome()
    context.driver.get(ahorcado_link)
    
 def after_all(context):
    print('Finalizando ATDD')
    context.driver.quit()
-
-@allure.step('Capturar pantalla')
-def capture_screenshot(context, step_name):
-    allure.attach(context.driver.get_screenshot_as_png(), name=step_name, attachment_type=AttachmentType.PNG)
-
-def after_step(context, step):
-    if step.status == "failed":
-        capture_screenshot(context, "screenshot")
--- automation/features/steps/ahorcado_steps.py
+++ automation/features/steps/ahorcado_steps.py
@@ -2,17 +2,19 @@ from behave import given, when, then, step
 import unittest
 from automation.ahorcado_page import AhorcadoPage
 
+class Test(unittest.TestCase):
 
-@given('I open ahorcado page')
-def i_am_on_the_homepage(context):
-    print("I am on the homepage")
-    # context.ahorcado_page = AhorcadoPage(context.driver)
-    # context.driver.get('https://www.youtube.com/')
-    
-@when('ingreso la letra {letra}')
-def ingreso_letra(context, letra):
-    print("Ingreso letra")
-
-@then('el juego me muestra {resultado}')
-def juego_muestra(context, resultado):
-    print("El juego me muestra resultado")
+    @given('I open ahorcado page')
+    def i_am_on_the_homepage(context):
+        print("I am on the homepage")
+        # context.ahorcado_page = AhorcadoPage(context.driver)
+        # context.driver.get('https://www.youtube.com/')
+        
+    @when('ingreso la letra {letra}')
+    def ingreso_letra(context, letra):
+        print("Ingreso letra")
+        context.assertEqual(letra, "a")
+        
+    @then('el juego me muestra {resultado}')
+    def juego_muestra(context, resultado):
+        print("El juego me muestra resultado")
